d4ab4f89e11936d11fb075a914a0d65d93c5227e
Fatma Cafri <33182036+fatmacafri@users.noreply.github.com>
2021-03-31 23:40:25 +0300

    Locatorlar güncellendi.

--- src/main/java/com/gittigidiyor/constants/ProductPageConstant.java
+++ src/main/java/com/gittigidiyor/constants/ProductPageConstant.java
@@ -13,8 +13,8 @@ public class ProductPageConstant
     public static final By PRODUCT_LIST=By.cssSelector("#best-match-right > div.blueWrapper.clearfix > div.clearfix > ul > li");
     public static final By PRODUCT_PRICE=By.id("sp-price-lowPrice");
     public static final By GO_TO_BASKET=By.cssSelector("div.basket-container>a");
-    public static final By BASKET_PRICE=By.xpath("//*[@class='total-price']");
-    public static final By NUMBER_OF_PRODUCT=By.xpath("//*[@class='amount']");
+    public static final By BASKET_PRICE=By.cssSelector("div.total-price>strong");
+    public static final By NUMBER_OF_PRODUCT=By.cssSelector("select.amount");
     public static final By DELETE_BUTTON=By.cssSelector("div>div>a.btn-delete>i.gg-icon");
     public static final By EMPTY_BASKET=By.cssSelector("div.empty-cart-info>div:nth-of-type(2)>h2");
     public static final By NUMBER_OF_PRODUCT_VALUE=By.xpath("//*[@class='amount']//*[@value='2']");
--- src/main/java/com/gittigidiyor/constants/SearchPageConstant.java
+++ src/main/java/com/gittigidiyor/constants/SearchPageConstant.java
@@ -7,9 +7,9 @@ public class SearchPageConstant
     public static String text="bilgisayar", url="https://www.gittigidiyor.com/arama/?k=bilgisayar&sf=2";
 
 
-    public static final By SEARCH =By.xpath("//*[@id=\"main-header\"]/div[3]/div/div/div/div[2]/form");
+    public static final By SEARCH =By.cssSelector("form>div>div[class='sc-1yew439-3 dkiUfE']");
     public static final By SEARCH_TEXT=By.name("k");
-    public static final By SEARCH_BUTTON=By.xpath("//*[@id=\"main-header\"]/div[3]/div/div/div/div[2]/form/div/div[2]/button");
-    public static final By PAGINATION =By.xpath("//*[@id=\"best-match-right\"]/div[5]/ul/li[2]/a");
+    public static final By SEARCH_BUTTON=By.cssSelector("div>button[data-cy='search-find-button']");
+    public static final By PAGINATION =By.cssSelector("div.pager>ul>li:nth-of-type(2)>a");
 
 }
